bf711612cf08fd04517da5770aa897a362b93ad9
Numan Syed <smnuman@gmail.com>
2025-06-25 00:15:29 +0600

    Set explicit leader key for Telescope bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,18 +5,43 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({
   { "dracula/vim", name = "dracula" },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { "neovim/nvim-lspconfig" },
   { "hrsh7th/nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp" },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } }
 })
+
 vim.cmd("colorscheme dracula")
 
 -- LSP Setup (e.g., for Python)
 local lspconfig = require("lspconfig")
 lspconfig.pyright.setup {}
+
+-- Completion Setup
+local cmp = require("cmp")
+cmp.setup({
+  sources = {
+    { name = "nvim_lsp" },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+})
+
+-- Treesitter Setup
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "python", "lua", "javascript" },
+  highlight = { enable = true },
+})
+
+-- Definitions
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Keybindings
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", {})
