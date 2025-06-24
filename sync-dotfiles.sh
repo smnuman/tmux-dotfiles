adca12103337c53d35b6b18bd2d49a6f6506dcad
Numan Syed <smnuman@gmail.com>
2025-06-25 00:42:49 +0600

    Enhance sync-dotfiles.sh to include more dotfiles

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-cp ~/.tmux.conf ~/tmux-dotfiles/tmux.conf
+# Sync dotfiles to repo
+cp ~/.tmux.conf ~/tmux-dotfiles/.tmux.conf
 cp ~/.tmux/plugins/tpm/scripts/helpers/tmux_echo_functions.sh ~/tmux-dotfiles/tpm/scripts/helpers/
+cp ~/.config/nvim/init.lua ~/tmux-dotfiles/nvim/init.lua
+cp ~/.zshrc ~/tmux-dotfiles/.zshrc
+cp ~/Library/Preferences/com.googlecode.iterm2.plist ~/tmux-dotfiles/iterm2.plist
+# Change directory to the dotfiles repo
 cd ~/tmux-dotfiles
-git add tmux.conf tpm/scripts/helpers/tmux_echo_functions.sh
-git commit -m "Sync tmux.conf and TPM scripts $(date)"
+# Add and commit changes
+git add .tmux.conf tpm/scripts/helpers/tmux_echo_functions.sh nvim/init.lua .zshrc iterm2.plist
+git commit -m "Sync dotfiles $(date)"
 git push origin main
